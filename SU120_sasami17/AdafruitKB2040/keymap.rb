c07293da1bd7a73967b2eb2ffe0af52f1986546f
alg <alg.info@gmail.com>
2022-02-10 01:39:16 +0900

    For KiCad

--- SU120_sasami17/AdafruitKB2040/keymap.rb
+++ SU120_sasami17/AdafruitKB2040/keymap.rb
@@ -1,6 +1,8 @@
 # Keyboard  : sasami17 (SU120)
 # Controller: Adafruit KB2040
 
+# for KiCad
+
 # Initialize a Keyboard ########################################################
 kbd = Keyboard.new
 
@@ -12,12 +14,11 @@ kbd.init_pins(
 
 # Initialize RGB ###############################################################
 class MyRgb < RGB
-  def toggle_color
-  	if @hue == 300
-  	  @hue = 60
-  	else
-  	  @hue = 300
-  	end
+  def to_raise_color
+    @hue = 60
+  end
+  def to_default_color
+    @hue = 300
   end
 end
 rgb = MyRgb.new(
@@ -38,20 +39,20 @@ kbd.append rgb # `kbd` is an instance of Keyboard class that should be newed in 
 # Rotary Encoder (D6)
 encoder_d6 = RotaryEncoder.new(28, 29)
 encoder_d6.clockwise do
-  kbd.send_key :KC_A
+  kbd.macro 'R', nil
 end
 encoder_d6.counterclockwise do
-  kbd.send_key :KC_B
+  kbd.macro 'r', nil
 end
 kbd.append encoder_d6
 
 # Rotary Encoder (E6)
 encoder_e6 = RotaryEncoder.new(9, 8)
 encoder_e6.clockwise do
-  kbd.send_key :KC_C
+  kbd.send_key :KC_F2
 end
 encoder_e6.counterclockwise do
-  kbd.send_key :KC_D
+  kbd.send_key :KC_F1
 end
 kbd.append encoder_e6
 
@@ -59,32 +60,42 @@ kbd.append encoder_e6
 # A         B         C         D         E
 # default layer should be added at first
 kbd.add_layer :default, %i(
-  KC_Q      KC_W      KC_E      KC_R      KC_T
-  KC_A      KC_S      KC_D      KC_F      KC_G
-  TGLCOLOR  RGB_SPI   RGB_SPD   RGB_HUI   RGB_HUD
+  TO_RAISE  KC_DEL    KC_ESC    LALT_3    LC_LS_A
+  KC_BSPC   LSFT_UP   KC_R      KC_V      LC_LS_C
+  LSFT_LEFT LSFT_DOWN LSFT_RGHT LSFT_X    LC_LS_L
   XXXXXXX   XXXXXXX   XXXXXXX   KC_4      KC_5
 )
 kbd.add_layer :raise, %i(
-  KC_1      KC_2      KC_3      KC_4      KC_5
-  KC_6      KC_7      KC_8      KC_9      KC_0
-  XXXXXXX   KC_X      KC_C      KC_V      KC_B
-  KC_SPC    KC_SPC    KC_SPC    KC_4      KC_5
+  TO_DEFLT  KC_DEL    KC_7      KC_8      KC_9
+  KC_BSPC   XXXXXXX   KC_4      KC_5      KC_6
+  KC_0      KC_DOT    KC_1      KC_2      KC_3
+  XXXXXXX   XXXXXXX   XXXXXXX   KC_4      KC_5
 )
 kbd.add_layer :adjust, %i(
-  KC_Q      KC_W      KC_E      KC_R      KC_T
-  KC_A      KC_S      KC_D      KC_F      KC_G
-  KC_Z      KC_X      KC_C      KC_V      KC_B
-  KC_SPC    KC_SPC    KC_SPC    KC_4      KC_5
+  KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
+  KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
+  KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
+  KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
 )
 kbd.add_layer :lower, %i(
-  KC_Q      KC_W      KC_E      KC_R      KC_T
-  KC_A      KC_S      KC_D      KC_F      KC_G
-  KC_Z      KC_X      KC_C      KC_V      KC_B
-  KC_SPC    KC_SPC    KC_SPC    KC_4      KC_5
+  KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
+  KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
+  KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
+  KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
 )
 
 ################################################################################
-kbd.define_mode_key :TGLCOLOR, [ Proc.new { rgb.toggle_color }, :raise, 300, nil ]
+kbd.define_composite_key :LSFT_UP,   %i(KC_UP    KC_LSFT)
+kbd.define_composite_key :LSFT_DOWN, %i(KC_DOWN  KC_LSFT)
+kbd.define_composite_key :LSFT_LEFT, %i(KC_LEFT  KC_LSFT)
+kbd.define_composite_key :LSFT_RGHT, %i(KC_RIGHT KC_LSFT)
+kbd.define_composite_key :LSFT_X,    %i(KC_X     KC_LSFT)
+kbd.define_composite_key :LALT_3,    %i(KC_3     KC_LALT)
+kbd.define_composite_key :LC_LS_A,   %i(KC_LCTL  KC_LSFT  KC_A)
+kbd.define_composite_key :LC_LS_C,   %i(KC_LCTL  KC_LSFT  KC_C)
+kbd.define_composite_key :LC_LS_L,   %i(KC_LCTL  KC_LSFT  KC_L)
+kbd.define_mode_key :TO_RAISE, [ Proc.new { rgb.to_raise_color   kbd.lock_layer(:raise)  }, nil, 300, nil ]
+kbd.define_mode_key :TO_DEFLT, [ Proc.new { rgb.to_default_color kbd.lock_layer(:default)}, nil, 300, nil ]
 
 # Start! #######################################################################
 kbd.start!
--- ShotgunCherryPie/PiPico/keymap.rb
+++ ShotgunCherryPie/PiPico/keymap.rb
@@ -16,39 +16,43 @@ kbd.add_layer :default, %i[
   RGB_TOG  KC_HOME  KC_END   KC_0
 ]
 
+# encoder1:zoom in/out
 encoder_1 = RotaryEncoder.new(27, 28)
 encoder_1.clockwise do
-  kbd.send_key :RGB_SPI
+  kbd.send_key :KC_F1
 end
 encoder_1.counterclockwise do
-  kbd.send_key :RGB_SPD
+  kbd.send_key :KC_F2
 end
 kbd.append encoder_1
 
+# encoder2:move left/right
 encoder_2 = RotaryEncoder.new(26, 22)
 encoder_2.clockwise do
-  kbd.send_key :RGB_VAI
+  kbd.send_key :KC_RIGHT
 end
 encoder_2.counterclockwise do
-  kbd.send_key :RGB_VAD
+  kbd.send_key :KC_LEFT
 end
 kbd.append encoder_2
 
+# encoder3:move up/down
 encoder_3 = RotaryEncoder.new(21, 20)
 encoder_3.clockwise do
-  kbd.send_key :RGB_SAI
+  kbd.send_key :KC_DOWN
 end
 encoder_3.counterclockwise do
-  kbd.send_key :RGB_SAD
+  kbd.send_key :KC_UP
 end
 kbd.append encoder_3
 
+# encoder4:rotate
 encoder_4 = RotaryEncoder.new(19, 18)
 encoder_4.clockwise do
-  kbd.send_key :RGB_HUI
+  kbd.macro 'R', nil
 end
-encoder_4.counterclockwise do
-  kbd.send_key :RGB_HUD
+encoder_3.counterclockwise do
+  kbd.macro 'r', nil
 end
 kbd.append encoder_4
 
@@ -67,7 +71,12 @@ rgb = RGB.new(
   0,
   false
 )
-rgb.effect = :swirl
+rgb.effect = :breath
+rgb.speed      = 31  # 1-31  / default: 22
+rgb.hue        = 300 # 0-100 / default: 0
+rgb.saturation = 100 # 0-100 / default: 100
+rgb.max_value  = 1   # 1-31  / default: 13
+
 kbd.append rgb
 
 kbd.start!
